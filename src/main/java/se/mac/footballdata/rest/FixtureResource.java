a412db862a734dff2f1f644856e09ca74ffe6260
Marcus Haglund <macowich@gmail.com>
2026-05-26 21:00:30 +0200

    Refaktoring igen

--- src/main/java/se/mac/footballdata/rest/FixtureResource.java
+++ src/main/java/se/mac/footballdata/rest/FixtureResource.java
@@ -1,16 +1,8 @@
 package se.mac.footballdata.rest;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
-import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-import org.bson.types.ObjectId;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
